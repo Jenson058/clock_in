626a6844eb79613f061879e719a3af3f8bc0ce59
Jenson <jensonf@163.com>
2024-08-24 18:45:33 +0800

    update

---
+++ app/src/pages/look/look.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+
+const weekList = ref(['日', '一', '二', '三', '四', '五', '六'])
+const year = ref(0)
+const month = ref(0)
+const weekDates = ref<Array<Array<number>>>([])
+
+onMounted(function () {
+  let day = this.$moment.format("YYYY-MM-DD")
+  year.value = day.getFullYear()
+  month.value = day.getMonth() + 1
+
+  day.setFullYear(year,month,1)
+  var w1 = day.getDay;
+})
+</script>
+
+<template>
+  <view class="view">
+    <view class="card">
+      <view class="year">{{year}}年{{month}}月</view>
+      <view class="week">
+        <view class="week-item" v-for="week in weekList">{{week}}</view>
+      </view>
+      <view></view>
+    </view>
+  </view>
+</template>
+
+<style scoped>
+.view {
+  box-sizing: border-box;
+  background: linear-gradient(#0287d2, white);
+  width: 100vw;
+  height: 95vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card{
+  background: white;
+  width: 90vw;
+  height: 50vh;
+  border-radius:5vw;
+}
+
+.year{
+  display: flex;
+  justify-content: center;
+}
+
+.week{
+  display: flex;
+  flex-direction: row;
+}
+
+.week-item{
+  flex: 1;
+  display: flex;
+  justify-content: center;
+}
+</style>
--- app/src/pages/transaction/transaction.vue
+++ app/src/pages/transaction/transaction.vue
@@ -1,14 +1,54 @@
 <template>
-  <view>
-
+  <view class="transition">
+    <up-text type="primary" text="基本"></up-text>
+    <up-grid
+        col="4"
+    >
+      <up-grid-item
+          v-for="item in transaction.basic"
+          @click="clickTransaction(item.path)"
+      >
+        <up-icon
+            :customStyle="{paddingTop:20+'rpx'}"
+            :name="item.icon"
+            :size="30"
+        ></up-icon>
+        <text class="grid-text">{{item.name}}</text>
+      </up-grid-item>
+    </up-grid>
   </view>
 </template>
 
 <script setup lang="ts">
 
+import {ref} from "vue";
+
+const transaction = ref({
+  basic:[
+      {name:"查看打卡",icon:"calendar",path:"/pages/look/look"}
+  ]
+})
+
+function clickTransaction(path){
+  uni.navigateTo({
+    url: path
+  })
+}
+
 </script>
 
 
 <style scoped lang="scss">
+.transition{
+  padding: 10px 30px 10px 30px;
+}
 
+.grid-text {
+  font-size: 14px;
+  color: #909399;
+  padding: 10rpx 0 20rpx 0rpx;
+  /* #ifndef APP-PLUS */
+  box-sizing: border-box;
+  /* #endif */
+}
 </style>
